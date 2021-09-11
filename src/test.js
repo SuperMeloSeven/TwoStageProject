5a1a8b6437811fc95fbaa8d55ed3aff4fd253107
huzhilei <924524413@qq.com>
2021-09-11 11:28:51 +0800

    style: 测试0911

---
+++ src/test.js
@@ -0,0 +1,6 @@
+let a = { headers: {'content': '111111'} }
+let b = { ...a, headers: {'content': '00000', 'auth': 'xsdads'} }
+let c = { headers: {'content': '00000', 'auth': 'xsdads'}, ...a }
+
+console.log(b, 'b')
+console.log(c, 'c')
